8512b716919b5027fcadd307da3fc8471eb26840
Mohan <102507601+mohan-springml@users.noreply.github.com>
2023-07-16 16:28:02 +0530

    Create DayIdentifierApplication.java

---
+++ src/main/java/com/example /DayIdentifierApplication.java
@@ -0,0 +1,12 @@
+package com.example.dayidentifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayIdentifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayIdentifierApplication.class, args);
+    }
+}
